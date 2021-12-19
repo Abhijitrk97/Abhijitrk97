9b67db2c059b4406f8766189820dde506f9d7fc6
Abhijitrk97 <95897040+Abhijitrk97@users.noreply.github.com>
2021-12-18 19:27:38 -0500

    Create sequences for the project.sql

---
+++ sequences for the project.sql
@@ -0,0 +1,32 @@
+create sequence seq_cust start with 1000
+increment by 1;
+
+create sequence seq_dlvptn start with 1000
+increment by 1;
+
+create sequence seq_supplier start with 1000
+increment by 1;
+
+create sequence seq_cat start with 100
+increment by 1;
+
+create sequence seq_prod start with 100
+increment by 1;
+
+create sequence seq_loc start with 100
+increment by 1;
+
+create sequence seq_dep start with 1
+increment by 1;
+
+create sequence seq_emp start with 1000
+increment by 1;
+
+create sequence seq_inventory start with 100
+increment by 1;
+
+create sequence seq_orders start with 1000
+increment by 1;
+
+create sequence seq_transaction start with 1000
+increment by 1;
