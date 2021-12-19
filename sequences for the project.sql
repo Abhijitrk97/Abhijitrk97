73711867572dba5aefc9b5a68a61396e59e62154
Abhijitrk97 <95897040+Abhijitrk97@users.noreply.github.com>
2021-12-18 20:18:04 -0500

    Update sequences for the project.sql

--- sequences for the project.sql
+++ sequences for the project.sql
@@ -1,3 +1,66 @@
+CREATE OR REPLACE FUNCTION getpartner
+RETURN deliveredby.partner_id%TYPE
+IS
+varpartner number:=0;
+BEGIN
+SELECT partner_id into varpartner from (SELECT partner_id from (SELECT count(partner_id), partner_id from deliveredby group by partner_id order by count(partner_id))fetch first 1 rows only);
+RETURN varpartner;
+END;
+/
+
+create or replace function getprice(
+varproductid number
+)
+return product.price%type
+is
+s_varproductprice number:=0;
+BEGIN
+
+SELECT price INTO s_varproductprice
+from product
+where productid = varproductid;
+
+RETURN s_varproductprice;
+
+END;
+/
+
+create or replace function getamount(
+varorderid number
+)
+return orders.amount%type
+is
+s_varorderid number:=0;
+s_varquantity number:=0;
+s_varproductid number:=0;
+s_varamout number:=0;
+BEGIN
+
+SELECT sum(product_price*quantity) INTO s_varamout
+from order_items
+where orderid = varorderid;
+
+RETURN s_varamout;
+
+END;
+/
+
+
+
+CREATE OR REPLACE FUNCTION is_numeric (str IN VARCHAR2)
+  RETURN INT
+IS
+  v_num NUMBER;
+BEGIN
+  v_num:=TO_NUMBER(str);
+  RETURN 1;
+EXCEPTION
+WHEN VALUE_ERROR THEN
+  RETURN 0;
+END is_numeric;
+/
+
+
 create sequence seq_cust start with 1000
 increment by 1;
 
@@ -11,6 +74,10 @@ create sequence seq_cat start with 100
 increment by 1;
 
 create sequence seq_prod start with 100
+increment by 1;
+create sequence seq_upc start with 100
+increment by 1;
+create sequence seq_sku start with 100
 increment by 1;
 
 create sequence seq_loc start with 100
@@ -30,3 +97,5 @@ increment by 1;
 
 create sequence seq_transaction start with 1000
 increment by 1;
+
+
